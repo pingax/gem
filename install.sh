9f8639342005b9101cdbacde0c08bdf8005f19a8
Aurélien Lubert <pacmiam@tuxfamily.org>
2020-04-30 20:27:21 +0200

    GEM - Update source code headers
    * Add the copyleft line to file headers. Five years, woh! :D
    * Clean little typo in license headers

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 # ------------------------------------------------------------------------------
+#  Copyleft 2015-2020  PacMiam
+#
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 3 of the License.
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
 #
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
